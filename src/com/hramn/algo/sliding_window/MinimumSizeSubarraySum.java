a89c493ed97281803fedc3cc4e224dd18d7d0e49
Dmitry Asaulenko <daa.life@proton.me>
2024-09-27 16:39:42 +0200

    209. Minimum Size Subarray Sum (Sliding Window)

---
+++ src/com/hramn/algo/sliding_window/MinimumSizeSubarraySum.java
@@ -0,0 +1,93 @@
+package com.hramn.algo.sliding_window;
+/**
+ * 209. Minimum Size Subarray Sum
+ * 
+ * Given an array of positive integers nums and a positive integer target, 
+ * return the minimal length of a subarray whose sum is greater than or equal 
+ * to target. If there is no such subarray, return 0 instead.
+ * 
+ * Example 1:
+ * Input: target = 7, nums = [2,3,1,2,4,3]
+ * Output: 2
+ * Explanation: The subarray [4,3] has the minimal length under the problem 
+ * constraint.
+ * 
+ * Example 2:
+ * Input: target = 4, nums = [1,4,4]
+ * Output: 1
+ * 
+ * Example 3:
+ * Input: target = 11, nums = [1,1,1,1,1,1,1,1]
+ * Output: 0
+ * 
+ * Constraints:
+ * 1 <= target <= 10**9
+ * 1 <= nums.length <= 10**5
+ * 1 <= nums[i] <= 10**4
+ * 
+ * Follow up: If you have figured out the O(n) solution, try coding another 
+ * solution of which the time complexity is O(n log(n)).
+ */
+
+// nums = [4,6,2,1,7,4]	target = 10	result = 2
+// nums = [5]	target = 6	result = 0
+// nums = [1, 2, 3, 4, 5]	target = 3	result = 1
+public class MinimumSizeSubarraySum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	class Solution {
+		public int minSubArrayLen(int target, int[] nums) {
+			int result = Integer.MAX_VALUE;
+			boolean isExist = false;
+			int l = 0, r = 0;
+			int sum = 0;
+			while (r < nums.length || sum >= target) {
+				if (sum >= target) {
+					result = Math.min(result, r - l);
+					isExist = true;
+					sum -= nums[l];
+					l++;
+				} else {
+					sum += nums[r];
+					r++;
+				}
+			}
+			return isExist ? result : 0;
+		}	
+	}
+	
+	class Solution_better {
+		public int minSubArrayLen(int target, int[] nums) {
+	        int l = 0;
+	        int r = 0;
+	        int s = 0;
+	        int ans = Integer.MAX_VALUE;
+	        boolean is = false;
+	        for (r = 0; r < nums.length; r++) {
+	            s += nums[r];
+	            while (s >= target) {
+	            	ans = Math.min(ans, r - l + 1);
+	            	is = true;
+	            	s -= nums[l++];
+	            }
+	        }
+	        return is ? ans : 0;
+	    }
+	}
+}
+
+//nums = [4,6,2,1,7,4]	target = 10	result = 2
+//1: sum + 4 = 4; r + 1 = 1
+//2: sum + 6 = 10; r + 1 = 2
+//3: result = 2; sum - 4 = 6; l + 1 = 1
+//4: sum + 2 = 8; r + 1 = 3
+//5: sum + 1 = 9; r + 1 = 4
+//6: sum + 7 = 16; r + 1 = 5
+//7: result = 2; sum - 6 = 10; l + 1 = 2
+//8: result = 2; sum - 2 = 8; l + 1 = 3
+//9: sum + 4 = 12; r + 1 = 6
+//10: result = 2
