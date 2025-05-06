0bfd2c615f9f8a3cb33e64077b1ab9c8d5a88b0d
Dmitry Asaulenko <daa.life@proton.me>
2025-05-06 11:15:44 +0200

    Challenge "Build Array from Permutation" + tests

---
+++ src/test/java/com/hramn/algo/arrays/BuildArrayFromPermutationTest.java
@@ -0,0 +1,59 @@
+package com.hramn.algo.arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Arrays;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class BuildArrayFromPermutationTest {
+
+	@Test
+	@DisplayName("nums = [0,2,1,5,3,4] -> [0,1,2,4,5,3]")
+	void testBruteforceSolutionBuildArray1() {
+		BuildArrayFromPermutation parent = new BuildArrayFromPermutation();
+		BuildArrayFromPermutation.BruteforceSolution solution 
+				= parent.new BruteforceSolution();
+		int[] result = solution.buildArray(new int[] {0,2,1,5,3,4});
+		assertNotNull(result);
+		assertEquals(6, result.length);
+		assertTrue(Arrays.equals(new int[] {0,1,2,4,5,3}, result));
+	}
+
+	@Test
+	@DisplayName("nums = [5,0,1,2,3,4] -> [4,5,0,1,2,3]")
+	void testBruteforceSolutionBuildArray2() {
+		BuildArrayFromPermutation parent = new BuildArrayFromPermutation();
+		BuildArrayFromPermutation.BruteforceSolution solution 
+				= parent.new BruteforceSolution();
+		int[] result = solution.buildArray(new int[] {5,0,1,2,3,4});
+		assertNotNull(result);
+		assertEquals(6, result.length);
+		assertTrue(Arrays.equals(new int[] {4,5,0,1,2,3}, result));
+	}
+
+	@Test
+	@DisplayName("nums = [0,2,1,5,3,4] -> [0,1,2,4,5,3]")
+	void testSolutionBuildArray1() {
+		BuildArrayFromPermutation parent = new BuildArrayFromPermutation();
+		BuildArrayFromPermutation.Solution solution = parent.new Solution();
+		int[] result = solution.buildArray(new int[] {0,2,1,5,3,4});
+		assertNotNull(result);
+		assertEquals(6, result.length);
+		assertTrue(Arrays.equals(new int[] {0,1,2,4,5,3}, result));
+	}
+
+	@Test
+	@DisplayName("nums = [5,0,1,2,3,4] -> [4,5,0,1,2,3]")
+	void testSolutionBuildArray2() {
+		BuildArrayFromPermutation parent = new BuildArrayFromPermutation();
+		BuildArrayFromPermutation.Solution solution = parent.new Solution();
+		int[] result = solution.buildArray(new int[] {5,0,1,2,3,4});
+		assertNotNull(result);
+		assertEquals(6, result.length);
+		assertTrue(Arrays.equals(new int[] {4,5,0,1,2,3}, result));
+	}
+}
